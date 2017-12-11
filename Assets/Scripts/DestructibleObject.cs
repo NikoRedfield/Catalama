40618bb98084acc54010b2c4631bc3c68fa5c513
Arnaud-PC\Arnaud <arnaudgarnier06@gmail.com>
2017-12-11 12:55:16 +0100

    Add the third controller Unity asset ; Add TPS camera that follows the player ; Modify jump heigh to allow player jump on cubes ; Add pv script to the scene game objects.

---
+++ Assets/Scripts/DestructibleObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObject : MonoBehaviour {
+    
+    [SerializeField]
+    private int pv;
+
+    public DestructibleObject()
+    {
+        pv = 0;
+    }
+
+    public DestructibleObject(int pv)
+    {
+        this.pv = pv;
+    }
+
+    private 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+}
