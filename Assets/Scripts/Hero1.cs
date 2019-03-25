c8b99bb094f38d91d4dc15e61e25683e1841743c
NikoRedfield <niko-redfield@hotmail.fr>
2019-03-25 18:56:45 +0100

    Getting back to the project

---
+++ Assets/Scripts/Hero1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero1 : MonoBehaviour {
+
+
+   // Vector3 rayOrigin = camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+    RaycastHit hit;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void SpawnCube()
+    {
+        //if(Input.GetButtonDown)
+    }
+}
---
+++ Assets/Scripts/LoadAfterTime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadAfterTime : MonoBehaviour
+{
+
+    public string _nextScene = "";
+
+    public float _delay = 5f;
+
+
+    //Load the given scene after the stated delay
+    public IEnumerator Start()
+    {
+        yield return new WaitForSeconds(_delay);
+        SceneManager.LoadScene(_nextScene);
+    }
+}
